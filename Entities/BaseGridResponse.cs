b3ec14defcf086de5d1e8d08ce84549e55da2ab4
Andrew Slusarev <jugius@gmail.com>
2022-04-29 19:23:23 +0300

    Базовые абстрактные классы

---
+++ Entities/BaseGridResponse.cs
@@ -0,0 +1,11 @@
+﻿using OutOfHome.DataProviders.Boards.Grids.Downloader.Entities.Common.Enums;
+
+namespace OutOfHome.DataProviders.Boards.Grids.Downloader.Entities
+{
+    public class BaseGridResponse : BaseResponse, Interfaces.IResponseContent
+    {
+        public string RawResponseString { get; set; }
+        public int Downloaded { get; set; }
+        public Language Language { get; set; }
+    }
+}
---
+++ Entities/BaseResponse.cs
@@ -0,0 +1,12 @@
+﻿using OutOfHome.DataProviders.Boards.Grids.Downloader.Entities.Common.Enums;
+
+
+namespace OutOfHome.DataProviders.Boards.Grids.Downloader.Entities
+{
+    public class BaseResponse : Interfaces.IResponse
+    {
+        public string RawQueryString { get; set; }
+        public Status? Status { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
