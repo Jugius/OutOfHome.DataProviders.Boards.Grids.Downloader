5a67d6470b213192b2206ed38daba4253f94be1d
Andrew Slusarev <jugius@gmail.com>
2022-04-30 17:18:05 +0300

    Вынести проект в отдельную папку

--- OutOfHome.DataProviders.Boards.Grids.Downloader/Entities/BaseGridResponse.cs
+++ OutOfHome.DataProviders.Boards.Grids.Downloader/Entities/BaseGridResponse.cs
@@ -1,9 +1,11 @@
 ﻿using OutOfHome.DataProviders.Boards.Grids.Downloader.Entities.Common.Enums;
+using System.Text.Json.Serialization;
 
 namespace OutOfHome.DataProviders.Boards.Grids.Downloader.Entities
 {
     public class BaseGridResponse : BaseResponse, Interfaces.IResponseContent
     {
+        [JsonIgnore]
         public string RawResponseString { get; set; }
         public int Downloaded { get; set; }
         public Language Language { get; set; }
--- OutOfHome.DataProviders.Boards.Grids.Downloader/Entities/BaseResponse.cs
+++ OutOfHome.DataProviders.Boards.Grids.Downloader/Entities/BaseResponse.cs
@@ -1,12 +1,17 @@
 ﻿using OutOfHome.DataProviders.Boards.Grids.Downloader.Entities.Common.Enums;
-
+using System.Text.Json.Serialization;
 
 namespace OutOfHome.DataProviders.Boards.Grids.Downloader.Entities
 {
     public class BaseResponse : Interfaces.IResponse
     {
+        [JsonIgnore]
         public string RawQueryString { get; set; }
+
+        [JsonIgnore]
         public Status? Status { get; set; }
+
+        [JsonIgnore]
         public string ErrorMessage { get; set; }
     }
 }
