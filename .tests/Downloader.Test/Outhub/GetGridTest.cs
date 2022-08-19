1301157c44948c6ed5f2a4f04c7973d2a5d8d59c
Andrew Slusarev <jugius@gmail.com>
2022-08-19 20:00:35 +0300

    Тесты

--- .tests/Downloader.Test/Outhub/GetGridTest.cs
+++ .tests/Downloader.Test/Outhub/GetGridTest.cs
@@ -1,9 +1,6 @@
-﻿using OutOfHome.DataProviders.Boards.Grids.Downloader;
+﻿using OutOfHome.DataProviders.Boards.Grids.Downloader.Entities.Bigmedia.GetGrid;
 using OutOfHome.DataProviders.Boards.Grids.Downloader.Entities.Outhub.GetGrid;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.Encodings.Web;
 using System.Text.Json;
 using System.Text.Unicode;
@@ -11,60 +8,61 @@ using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
 
-namespace Downloader.Test.Outhub
+namespace Downloader.Test.Outhub;
+
+public class GetGridTest
 {
-    public class GetGridTest
+    private readonly ITestOutputHelper output;
+    private OutOfHome.DataProviders.Boards.Grids.Downloader.OuthubHttpEngine outhubService;
+    public GetGridTest(ITestOutputHelper output)
     {
-        private readonly ITestOutputHelper output;
-        public GetGridTest(ITestOutputHelper output)
-        {
-            this.output = output;
-        }
+        this.output = output;
+        this.outhubService = new OutOfHome.DataProviders.Boards.Grids.Downloader.OuthubHttpEngine();
+    }
 
-        [Fact]
-        public async Task DownloadResponse()
-        {
-            GetGridRequest request = new GetGridRequest();
-            var s = request.GetUri();
-            output.WriteLine(s.ToString());
-            var response = await OutOfHome.DataProviders.Boards.Grids.Downloader.Outhub.GetGrid.QueryAsync(request);
+    [Fact]
+    public async Task DownloadResponse()
+    {
+        GetGridRequest request = new GetGridRequest();
+        var s = request.GetUri();
+        output.WriteLine("Request url: " + s.ToString());
+        var response = await outhubService.GetGrid.QueryAsync(request);
 
-            Assert.NotNull(response);
-            var options = new JsonSerializerOptions
-            {
-                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
-                WriteIndented = true
-            };
-            
-            using (var stream = System.IO.File.Create(@"d:\MyFiles\GridDownloader Responses\tests\response.json"))
-            {
-                await System.Text.Json.JsonSerializer.SerializeAsync(stream, response, options);
-            }
-            
-        }
-        [Fact]
-        public async Task OpenResponseFromFile()
+        Assert.NotNull(response);
+        var options = new JsonSerializerOptions
         {
-            GetGridResponse response;
-            var options = new JsonSerializerOptions
-            {
-                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
-                WriteIndented = true
-            };
-
-            using (var stream = System.IO.File.OpenRead(@"d:\MyFiles\GridDownloader Responses\tests\response.json"))
-            {
-                response = await JsonSerializer.DeserializeAsync<GetGridResponse>(stream, options);
-            }
-            Assert.True(response.Boards.Length > 0);
-            output.WriteLine(response.Boards[0]);
-            output.WriteLine(GetAddress(response.Boards[1]));
-
-        }
-        private static string GetAddress(string board)
+            Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+            WriteIndented = true
+        };
+        
+        using (var stream = System.IO.File.Create(@"d:\MyFiles\GridDownloader Responses\tests\response.json"))
         {
-            ReadOnlySpan<string> splitted = board.Split(';');
-            return splitted[15];
+            await System.Text.Json.JsonSerializer.SerializeAsync(stream, response, options);
         }
+        
+    }
+    [Fact]
+    public async Task OpenResponseFromFile()
+    {
+        GetGridResponse response;
+        var options = new JsonSerializerOptions
+        {
+            Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+            WriteIndented = true
+        };
+
+        using (var stream = System.IO.File.OpenRead(@"d:\MyFiles\GridDownloader Responses\tests\response.json"))
+        {
+            response = await JsonSerializer.DeserializeAsync<GetGridResponse>(stream, options);
+        }
+        Assert.True(response.Result.Boards.Length > 0);
+        output.WriteLine(response.Result.Boards[0]);
+        output.WriteLine(GetAddress(response.Result.Boards[1]));
+
+    }
+    private static string GetAddress(string board)
+    {
+        ReadOnlySpan<string> splitted = board.Split(';');
+        return splitted[15];
     }
 }
