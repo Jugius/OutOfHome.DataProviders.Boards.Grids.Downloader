c7b19875ac82204353961de4ac40d922c9082136
Andrew Slusarev <jugius@gmail.com>
2022-10-25 16:53:22 +0300

    Обработать ошибку сервера при загрузке Outhub респонса

--- OutOfHome.DataProviders.Boards.Grids.Downloader/Entities/Outhub/ContentParser.cs
+++ OutOfHome.DataProviders.Boards.Grids.Downloader/Entities/Outhub/ContentParser.cs
@@ -4,6 +4,12 @@ namespace OutOfHome.DataProviders.Boards.Grids.Downloader.Entities.Outhub;
 
 internal class ContentParser : Interfaces.IContentParser<ResponseContent>
 {
-    public async Task<ResponseContent> ParseContent(HttpResponseMessage message) =>
-        await System.Text.Json.JsonSerializer.DeserializeAsync<ResponseContent>(await message.Content.ReadAsStreamAsync());
+    public async Task<ResponseContent> ParseContent(HttpResponseMessage message)
+    { 
+        var content = await System.Text.Json.JsonSerializer.DeserializeAsync<ResponseContent>(await message.Content.ReadAsStreamAsync());
+        if (content.ServerStatus < 0)
+            throw new Exceptions.DownloaderException(Exceptions.ErrorCode.ServerError, $"Cервер вернул статус: {content.ServerStatus}");
+        return content;
+    }
+        
 }
