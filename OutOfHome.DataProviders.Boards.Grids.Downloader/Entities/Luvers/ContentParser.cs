f57d9058145a510263be51f41dbf93ee486f6ae5
Andrew Slusarev <jugius@gmail.com>
2024-03-06 14:42:01 +0200

    Добавить оператора Luvers

---
+++ OutOfHome.DataProviders.Boards.Grids.Downloader/Entities/Luvers/ContentParser.cs
@@ -0,0 +1,10 @@
+﻿
+namespace OutOfHome.DataProviders.Boards.Grids.Downloader.Entities.Luvers;
+internal class ContentParser : Interfaces.IContentParser<string[]>
+{
+    public async Task<string[]> ParseContent(HttpResponseMessage message)
+    {
+        var response = await message.Content.ReadAsStringAsync();
+        return response.Split(Environment.NewLine);
+    }
+}
---
+++ OutOfHome.DataProviders.Boards.Grids.Downloader/Entities/Luvers/GetSpecRequest.cs
@@ -0,0 +1,8 @@
+﻿using OutOfHome.DataProviders.Boards.Grids.Downloader.Entities.Interfaces;
+
+namespace OutOfHome.DataProviders.Boards.Grids.Downloader.Entities.Luvers;
+internal class GetSpecRequest : IRequest
+{
+    private const string uri = @"https://db.luvers.com.ua/spec.php";
+    public Uri GetUri() => new Uri(uri);
+}
