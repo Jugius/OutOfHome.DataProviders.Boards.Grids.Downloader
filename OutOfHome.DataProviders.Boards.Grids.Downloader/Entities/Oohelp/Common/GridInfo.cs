54249ccb38cbc1e032c804d262bdb5eff4ea9e0b
Andrew Slusarev <jugius@gmail.com>
2024-07-17 19:33:44 +0300

    Updates oohelp api models

--- OutOfHome.DataProviders.Boards.Grids.Downloader/Entities/Oohelp/Common/GridInfo.cs
+++ OutOfHome.DataProviders.Boards.Grids.Downloader/Entities/Oohelp/Common/GridInfo.cs
@@ -4,22 +4,8 @@ using System.Text.Json.Serialization;
 namespace OutOfHome.DataProviders.Boards.Grids.Downloader.Entities.Oohelp.Common;
 public class GridInfo
 {
-    [JsonPropertyName("id")]
     public Guid Id { get; set; }
-
-    [JsonPropertyName("provider")]
     public ProviderInfo Provider { get; set; }
-
-
-    [JsonPropertyName("downloaded")]
     public DateTime Downloaded { get; set; }
-
-
-    [JsonConverter(typeof(JsonStringEnumConverter))]
-    [JsonPropertyName("language")]
-    public Language Language { get; set; }
-
-
-    [JsonPropertyName("count")]
     public int BoardsCount { get; set; }
 }
--- OutOfHome.DataProviders.Boards.Grids.Downloader/Entities/Oohelp/Common/ProviderInfo.cs
+++ OutOfHome.DataProviders.Boards.Grids.Downloader/Entities/Oohelp/Common/ProviderInfo.cs
@@ -1,16 +1,7 @@
-﻿using System.Text.Json.Serialization;
-
+﻿
 namespace OutOfHome.DataProviders.Boards.Grids.Downloader.Entities.Oohelp.Common;
 public class ProviderInfo
-{
-    [JsonPropertyName("id")]
+{  
     public int Id { get; set; }
-
-
-    [JsonPropertyName("name")]
     public string Name { get; set; }
-
-
-    [JsonPropertyName("formatted")]
-    public string FormattedName { get; set; }
 }
