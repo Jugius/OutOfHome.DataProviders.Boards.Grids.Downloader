60a56ed9a7a77ed366716a26582b039e793154ef
Andrew Slusarev <jugius@gmail.com>
2025-02-17 19:06:51 +0200

    GetQueryParameters

--- OutOfHome.DataProviders.Boards.Grids.Downloader/Entities/Oohelp/GetActualGridsRequest.cs
+++ OutOfHome.DataProviders.Boards.Grids.Downloader/Entities/Oohelp/GetActualGridsRequest.cs
@@ -8,17 +8,23 @@ public class GetActualGridsRequest : IRequest
     public string Key { get; set; }
     public virtual Uri GetUri()
     {
-        List<string> parameters = new List<string>();
-        
-        if (this.GridsDownoadedDate.HasValue)
-            parameters.Add($"date={GridsDownoadedDate:yyyy.MM.dd}");
-
-        if(!string.IsNullOrEmpty(this.Key))
-            parameters.Add($"key={this.Key}");
+        var parameters = GetQueryParameters();
 
         if(parameters.Count == 0)
             return new Uri(baseUri);
 
         return new Uri($"{baseUri}?{string.Join('&', parameters)}");        
+    }
+    protected List<string> GetQueryParameters()
+    {
+        List<string> parameters = new List<string>(2);
+
+        if (this.GridsDownoadedDate.HasValue)
+            parameters.Add($"date={GridsDownoadedDate:yyyy.MM.dd}");
+
+        if (!string.IsNullOrEmpty(this.Key))
+            parameters.Add($"key={this.Key}");
+        
+        return parameters;
     }
 }
