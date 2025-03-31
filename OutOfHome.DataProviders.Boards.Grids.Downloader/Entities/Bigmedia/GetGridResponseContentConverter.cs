1ef48bab1f2d5180576789be71429f15a6c79ef8
Andrew Slusarev <jugius@gmail.com>
2025-03-31 17:18:49 +0300

    Refactor

--- OutOfHome.DataProviders.Boards.Grids.Downloader/Entities/Bigmedia/GetGridResponseContentConverter.cs
+++ OutOfHome.DataProviders.Boards.Grids.Downloader/Entities/Bigmedia/GetGridResponseContentConverter.cs
@@ -1,71 +1,29 @@
 ﻿using OutOfHome.DataProviders.Boards.Grids.Downloader.Entities.Bigmedia.Common;
-using System.Text.Json.Serialization;
+using OutOfHome.DataProviders.Boards.Grids.Downloader.Entities.Bigmedia.Extentions;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace OutOfHome.DataProviders.Boards.Grids.Downloader.Entities.Bigmedia;
 internal class GetGridResponseContentConverter : Interfaces.IResponseConverter<List<Board>>
 {
     public async Task<List<Board>> Convert(HttpResponseMessage message)
     {
-        var jsonOptions = new JsonSerializerOptions()
-        {
-            NumberHandling = JsonNumberHandling.AllowReadingFromString
-        };
-
-        Board[] boards;
+        List<Board> boards = new List<Board>();
 
         using (var readingStream = await message.Content.ReadAsStreamAsync())
         {
-            try
-            {
-                boards = await JsonSerializer.DeserializeAsync<Board[]>(readingStream, jsonOptions);
-                
-                if (boards == null || boards.Length == 0)
-                    throw new Exceptions.DownloaderException(Exceptions.ErrorCode.ServerError, $"Cервер вернул 0 записей.");
-            }
-            catch (JsonException)
-            {                
-                readingStream.Position = 0;
-                var internalBoards = await JsonSerializer.DeserializeAsync<InternalBmaBoard[]>(await message.Content.ReadAsStreamAsync(), jsonOptions);
-                boards = FilterAndConvertToBoards(internalBoards).ToArray();
-            }
-        }    
+            boards = await ConvertToResponse(readingStream);
+        }
+
+        if (boards.Count == 0)
+            throw new Exceptions.DownloaderException(Exceptions.ErrorCode.ServerError, $"Cервер вернул 0 записей.");
 
         foreach (var brd in boards)
         {
             brd.PhotoUrl = NormalizeUri(brd.PhotoUrl);
             brd.SchemaUrl = NormalizeUri(brd.SchemaUrl);
         }
-        return boards.ToList();
-    }
-    private static IEnumerable<Board> FilterAndConvertToBoards(IEnumerable<InternalBmaBoard> boards)
-    {
-        foreach (var b in boards)
-        {
-            if (b.Lat.GetValueOrDefault(0) == 0 || b.Lon.GetValueOrDefault(0) == 0)
-                continue;
-
-            yield return new Board
-            {
-                Address = b.Address,
-                Angle = b.Angle,
-                Grp = b.Grp,
-                IdCatab = b.IdCatab,
-                IdCity = b.IdCity,
-                IdNetwork = b.IdNetwork,
-                IdSize = b.IdSize,
-                IdSupplier = b.IdSupplier,
-                Lat = b.Lat,
-                Lon = b.Lon,
-                Light = b.Light,
-                Ots = b.Ots,
-                PhotoUrl = b.PhotoUrl,
-                Price = b.Price,
-                SchemaUrl = b.SchemaUrl,
-                Sides = b.Sides,
-                SupplierSidetype = b.SupplierSidetype,
-            };
-        }
+        return boards;
     }
     private static string NormalizeUri(string uri)
     {
@@ -76,4 +34,19 @@ internal class GetGridResponseContentConverter : Interfaces.IResponseConverter<L
 
         return Uri.TryCreate(uri, UriKind.Absolute, out Uri u) ? u.ToString() : null;
     }
+    public static async Task<List<Board>> ConvertToResponse(Stream stream)
+    {
+        var jsonOptions = new JsonSerializerOptions() { NumberHandling = JsonNumberHandling.AllowReadingFromString };
+
+        try
+        {
+            return await JsonSerializer.DeserializeAsync<List<Board>>(stream, jsonOptions);
+        }
+        catch (JsonException)
+        {
+            stream.Position = 0;
+            var internalBoards = await JsonSerializer.DeserializeAsync<InternalBmaBoard[]>(stream, jsonOptions);
+            return internalBoards.Select(board => board.MapToBoard()).ToList();
+        }
+    }    
 }
